e5057698c8013ebf14a21bfe1d6e35b1ebd253dc
aso4 <albertyso@gmail.com>
2017-04-04 12:43:38 -0400

    add java folder

---
+++ java/algorithms/src/com/company/BubbleSort.java
@@ -0,0 +1,25 @@
+public class BubbleSort {
+
+    public void example (){
+        int[] array = {1, 2, 3, 4, 5};
+
+
+        for (int i = 0; i <= array.length - 2; i++) {
+            for (int j = 0; j <= array.length - 2 - i; j++) {
+                System.out.println("iterating. j = " + j);
+
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        System.out.printf("final array is :");
+        for (int x = 0; x < array.length; x++) {
+
+            System.out.printf(array[x] + ",");
+        }
+    }
+
+}
---
+++ java/algorithms/src/com/company/SelectionSort.java
@@ -0,0 +1,8 @@
+package com.company;
+
+/**
+ * Created by albertso on 4/4/17.
+ */
+public class selectionsort {
+
+}
