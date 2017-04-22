0582a861f5da13e42f82951d3d7cc24b419337e8
aso4 <albertyso@gmail.com>
2017-04-21 23:35:53 -0400

    StringToNumber

---
+++ java/algorithms/src/com/company/StringToNumber.java
@@ -0,0 +1,18 @@
+package com.company;
+
+/**
+ * Created by albertso on 4/21/17.
+ */
+public class StringToNumber {
+    public static int stringToNumber(String str) {
+        return Integer.parseInt(str);
+    }
+}
+
+/*
+public class StringToNumber {
+    public static int stringToNumber(String str) {
+        return Integer.valueOf(str); // use of valueOf
+    }
+}
+*/
