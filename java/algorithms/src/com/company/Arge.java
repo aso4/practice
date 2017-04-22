cd2dd4526b79818f7a1647815af0d0ea9ef6b505
aso4 <albertyso@gmail.com>
2017-04-21 23:15:54 -0400

    add comments and other solutions to popgrowth problem

--- java/algorithms/src/com/company/Arge.java
+++ java/algorithms/src/com/company/Arge.java
@@ -16,4 +16,24 @@ class Arge {
     }
 }
 
-// https://www.codewars.com/kata/563b662a59afc2b5120000c6/train/java
+/*
+public class Arge {
+    public static int nbYear(int p0, double percent, int aug, int p) {
+        int years = 0;
+        while (p0 < p) {
+            p0 += p0 * percent / 100 + aug; // += casts to int and auto floors the result.
+            years++;
+        }
+        return years;
+    }
+}*/
+
+/*
+class Arge {
+    public static int nbYear(int p0, double percent, int aug, int p) {
+        int y = 0;
+        double c = p0;
+        for(; c < p; y++) c = c * (1 + percent / 100D) + aug; // 100D converts 100 to double. no parameter instantiated for loop
+        return y;
+    }
+}*/
