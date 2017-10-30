05ff1d948b3eaa4556b9b32a22c01760fe9b793f
aso4 <albertyso@gmail.com>
2017-10-30 00:44:52 -0400

    day 10 edit

--- csharp/hr-d10.cs
+++ csharp/hr-d10.cs
@@ -6,16 +6,37 @@ class Solution {
 
     static void Main(String[] args) {
         int n = Convert.ToInt32(Console.ReadLine());
-        int remainder;
-        string binary;
-        
+        int remainder = 0;
+        int max = 0;
+        Stack<string> binary = new Stack<string>();
+        String s = "";
+
         while(n > 0) {
             remainder = n%2;
             n = n/2;
-            binary += remainder.ToString(); // Insert remainder to front of a list or push onto a stack 
+            binary.Push(remainder.ToString()); // Insert remainder to front of a list or push onto a stack
+            s += remainder.ToString();
         }
 
-        Console.Write(binary);
-        // Print list or stack
+        // Console.Write(s);
+        s = binary.ToString();
+        Console.Write(s);
+
+        while (binary.Count > 0) {
+            int temp = 0;
+            Console.WriteLine(binary.Peek());
+            if (binary.Pop().Equals('1')) {
+                temp++;
+
+            } else {
+                temp = 0;
+            }
+
+            if (temp > max) {
+                max = temp;
+            }
+        }
+
+        Console.Write(max);
     }
 }
