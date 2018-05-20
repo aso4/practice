46a0a507b7effb58d12ea7908db87c6ab097a0da
aso4 <albertyso@gmail.com>
2018-05-19 23:14:50 -0400

    partial implement caesarcipher

--- java/Cipher/CaesarCipher.java
+++ java/Cipher/CaesarCipher.java
@@ -44,4 +44,36 @@ public class CaesarCipher {
         System.out.println(encrypt("First Legion", 17));
 
     }
+
+    private String encryptTwoKeys(String input, int key1, int key2) {
+        StringBuilder encrypted = new StringBuilder(input);
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String shiftedAlphabet1 = alphabet.substring(key1) + alphabet.substring(0, key1);
+        String shiftedAlphabet2 = alphabet.substring(key2) + alphabet.substring(0, key2);
+        char newChar;
+        for (int i = 0; i < encrypted.length(); i++) {
+            char currChar = encrypted.charAt(i);
+            int idx = alphabet.indexOf(Character.toUpperCase(currChar));
+            boolean isLowerCase = Character.isLowerCase(currChar);
+
+            if (idx != -1) {
+                if (idx % 2 == 0) {
+                    newChar = Character.toLowerCase(shiftedAlphabet2.charAt(idx));
+                } else {
+                    newChar = Character.toLowerCase(shiftedAlphabet1.charAt(idx));
+                }
+            }
+            if (isLowerCase) {
+                newChar = Character.toLowerCase(newChar);
+            }
+            encrypted.setCharAt(i, newChar);
+        }
+        return encrypted.toString();
+    }
+
+    public void testEncryptTwoKeys() {
+        System.out.println(encryptTwoKeys("First Legion", 23, 17));
+        // Czojq Ivdzle
+        // actual: Czojq Ivxzfk
+    }
 }
